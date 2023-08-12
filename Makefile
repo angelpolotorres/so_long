6841c7115445ba48d70230617dfb6742787e36ad
Angel Polo <angelpolotorres@gmail.com>
2023-08-12 20:59:46 +0200

    corrigiendo merge

---
+++ Makefile
@@ -0,0 +1,69 @@
+
+# Final program ---------------------------------------------- #
+# ------------------------------------------------------------ #
+NAME = so_long
+
+# Source files ----------------------------------------------- #
+# ------------------------------------------------------------ #
+SRCS = $(SRC_FILES) $(SRC_GNL_FILES)
+
+SRC_DIR = src
+SRC_FILES = $(addprefix $(SRC_DIR)/, $(SRC_CFILES))
+SRC_CFILES = \
+		so_long.c \
+		read_map.c \
+		check_map.c \
+		check_path.c \
+		load_graphics.c \
+		manage_errors.c \
+
+SRC_GNL_DIR = $(SRC_DIR)/gnl
+SRC_GNL_FILES = $(addprefix $(SRC_GNL_DIR)/, $(SRC_GNL_CFILES))
+SRC_GNL_CFILES = \
+		get_next_line.c \
+		get_next_line_utils.c \
+
+MLX_DIR = $(SRC_DIR)/mlx
+
+# Object files ----------------------------------------------- #
+# ------------------------------------------------------------ #
+OBJS = $(patsubst $(SRC_DIR)/%.c,$(OBJ_DIR)/%.o,$(SRCS))
+
+OBJ_DIR = build
+OBJ_GNL_DIR = $(OBJ_DIR)/gnl
+
+# Compiler options ------------------------------------------- #
+# ------------------------------------------------------------ #
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -g -fsanitize=address
+LDFLAGS := -L./$(MLX_DIR) -lmlx -framework OpenGL -framework AppKit
+INCL_DIR := includes
+
+# Rules ------------------------------------------------------ #
+# ------------------------------------------------------------ #
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(MAKE) -C $(MLX_DIR)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $(NAME) $(OBJS)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR) $(OBJ_GNL_DIR) 
+	$(CC) $(CFLAGS) -I./$(INCL_DIR) -c $< -o $@
+
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+$(OBJ_GNL_DIR):
+	mkdir -p $(OBJ_GNL_DIR)
+
+clean:
+	rm -rf $(OBJ_DIR)
+#	$(MAKE) -C $(MLX_DIR) clean 
+#	Activar si quieres eliminar los .o de mlx con el clean
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ src/load_graphics.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_graphics.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apolo-to <apolo-to@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 13:43:08 by apolo-to          #+#    #+#             */
+/*   Updated: 2023/08/12 16:58:27 by apolo-to         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include "mlx.h"
+
+// ft_load_xpm_to_game
+// ft_print_graphs_in_map
+
+/**
+ * This ft load the xpm files to game structure using mlx library
+ * @param t_game* Game structure
+ * @return void
+*/
+static void	ft_load_xpm_to_game(t_game *game)
+{
+	int	width;
+	int	height;
+
+	game->wall_img = mlx_xpm_file_to_image(game->mlx_ptr, "data/imgs/wall.xpm", &width, &height);
+	game->floor_img = mlx_xpm_file_to_image(game->mlx_ptr, "data/imgs/floor.xpm", &width, &height);
+	game->collectable_img = mlx_xpm_file_to_image(game->mlx_ptr, "data/imgs/point.xpm", &width, &height);
+	game->player_img = mlx_xpm_file_to_image(game->mlx_ptr, "data/imgs/player.xpm", &width, &height);
+	game->exit_img = mlx_xpm_file_to_image(game->mlx_ptr, "data/imgs/exit.xpm", &width, &height);
+}
+
+static void	ft_print_map_with_graphics(t_game *game)
+{
+	int	row;
+	int	col;
+
+	row = 0;
+	while (row < game->map_rows)
+	{
+		col = 0;
+		while (col < game->map_cols)
+		{
+			if (game->map[row][col] == '1')
+				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->wall_img, col * 50, row * 50);
+			if (game->map[row][col] == '0')
+				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->floor_img, col * 50, row * 50);
+			if (game->map[row][col] == 'P')
+				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->player_img, col * 50, row * 50);
+			if (game->map[row][col] == 'C')
+				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->collectable_img, col * 50, row * 50);
+			if (game->map[row][col] == 'E')
+				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->exit_img, col * 50, row * 50);
+			col++;
+		}
+		row++;
+	}
+}
+
+void	ft_print_graphics_in_window(t_game *game)
+{
+	ft_load_xpm_to_game(game);
+	ft_print_map_with_graphics(game);
+}
