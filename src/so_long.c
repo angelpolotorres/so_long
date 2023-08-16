eabc5e8f425101529aeb599198d8c24a0af05bea
Angel Polo Torres <apolo-to@c1r9s4.42madrid.com>
2023-08-16 10:30:08 +0200

    corregido nombre de la ventana

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: apolo-to <apolo-to@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 12:40:53 by apolo-to          #+#    #+#             */
-/*   Updated: 2023/08/15 13:14:32 by apolo-to         ###   ########.fr       */
+/*   Updated: 2023/08/16 10:29:50 by apolo-to         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	main(int argc, char **argv)
 	ft_check_map_errors(&game, argv);
 	game.mlx_ptr = mlx_init();
 	game.win_ptr = mlx_new_window(game.mlx_ptr, (game.map_cols * 50),
-			(game.map_rows * 50), "so_long_game");
+			(game.map_rows * 50), "so_long");
 	ft_print_graphics_in_window(&game);
 	mlx_string_put(game.mlx_ptr, game.win_ptr, 10, 20, 0x4d4282, str);
 	mlx_key_hook(game.win_ptr, ft_controls, &game);
